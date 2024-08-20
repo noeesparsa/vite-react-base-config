dbafd69b0e6f1dac74a5d50a547deec41758c884
Rangoow <noe.esp@hotmail.fr>
2024-08-20 09:41:23 +0200

    update initial config

--- jest-extended.d.ts
+++ jest-extended.d.ts
@@ -2,9 +2,9 @@ import type CustomMatchers from "jest-extended";
 import "vitest";
 
 declare module "vitest" {
-  type Assertion<T = any> = NonNullable<unknown> & CustomMatchers<T>;
+  type Assertion<T = unknown> = NonNullable<unknown> & CustomMatchers<T>;
 
-  type AsymmetricMatchersContaining<T = any> = NonNullable<unknown> &
+  type AsymmetricMatchersContaining<T = unknown> = NonNullable<unknown> &
     CustomMatchers<T>;
 
   type ExpectStatic = NonNullable<unknown> & CustomMatchers<T>;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./app/App.tsx";
 import "./index.css";
+import App from "./app/App";
 
-createRoot(document.getElementById("root")!).render(
+import "./i18n";
+
+const root = createRoot(document.getElementById("root") as HTMLElement);
+
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,
